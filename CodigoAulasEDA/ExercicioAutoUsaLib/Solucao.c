af8d0e5aaff4870e1eb18472f9e4aee8d71267e9
lufer <lufer@ipca.pt>
2023-02-23 11:28:55 +0000

    Consolidação PI

---
+++ CodigoAulasEDA/ExercicioAutoUsaLib/Solucao.c
@@ -0,0 +1,17 @@
+/*****************************************************************//**
+ * @file   Solucao.c
+ * @brief  
+ * 
+ * @author lufer
+ * @date   February 2023
+ *********************************************************************/
+#include <stdio.h>
+#include "Funcs.h"
+
+#pragma comment(lib,"ExercicioAuto")
+
+int main(){
+
+	int x = Sum(2, 3);
+	printf("%d\n", x);
+}
