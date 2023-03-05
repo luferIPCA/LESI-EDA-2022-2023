82bfcdf171c25a10aabe32bd9009c5275ce9d604
lufer <lufer@ipca.pt>
2023-03-05 21:59:56 +0000

    Resolução Folha Exercicios II

---
+++ CodigoAulasEDA/Aula 2 - Apontadores/Pointers.c
@@ -0,0 +1,52 @@
+/*
+* author: lufer
+* email: lufer@ipca.pt
+* date: 06.03.2022
+* desc: Apontadores
+*/
+
+#include "Dados.h"
+
+//int* changeValue(const int* p) {
+			//não é permitido pois é const
+int changeValue( int* p) {
+	*p = *p + 1;		
+	return *p;
+}
+
+int Maior(int* p, int* q) {
+	if (*p > *q) return *p;
+	return *q;
+}
+
+bool TrocaII(int* p, int* q) {
+	//return false;
+	
+	return true;
+}
+
+/**
+* Verifica se determinado existe num array
+* @param [in] n
+* @param [out] p	//posição do array
+*/
+bool ExisteValorPosicao(int v[], int n, int valor, int* pos) {
+
+	for (int i = 0; i < n; i++) {
+		if (v[i] == valor) {
+			*pos = i;		//posição onde existe
+			return (true);
+		}
+	}
+	*pos = -1;				//posição errada!
+	return (false);
+}
+
+/**
+* Analisar!!!! Qual o problema?
+*/
+int get(int* ptr) {
+	int a = 10;
+	ptr = &a;
+	return 0;
+}
