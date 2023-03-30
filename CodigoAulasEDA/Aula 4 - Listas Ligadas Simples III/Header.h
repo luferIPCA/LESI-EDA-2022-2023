fd9e20135442abc2923d294ff583f8d1ab93c455
lufer <lufer@ipca.pt>
2023-03-30 16:16:30 +0100

    Listas Ligadas com **

--- CodigoAulasEDA/Aula 4 - Listas Ligadas Simples III/Header.h
+++ CodigoAulasEDA/Aula 4 - Listas Ligadas Simples III/Header.h
@@ -6,6 +6,7 @@
  * @date   March 2023
  *********************************************************************/
 #pragma once
+#include <stdbool.h>
 
 typedef struct Cliente{
 	int num;
@@ -16,5 +17,22 @@ typedef struct ClienteLista {
 	Cliente c;
 	struct ClienteLista* next;
 }ClienteLista;
+/**
+ * Altera Lista e devolve resultado da operação.
+ * Recebe um apontador para struct
+ */
+bool InsertNode(ClienteLista** h, Cliente* novo);
+
+/**
+ * Altera Lista, devolve resultado
+ * Segue padrão SOLID (S: "Single Responsability")
+ * O método apenas gera a lista. O novo elemento entra criado.
+ * Recebe um apontador para struct da lista
+ */
+bool InsertNodeII(ClienteLista** h, ClienteLista* novo);
+
+void MostraLista(ClienteLista* h);
+
+
 
 
